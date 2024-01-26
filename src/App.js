050a69a5cf13b01605919b7c24173baf96719267
ManishaRamar <rasamaha252@gmail.com>
2024-01-26 16:06:01 +0530

    kth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+// import {BrowserRouter, Routes, Route} from "react-router-dom"
 
-import Layout from "./Layout"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Service from "./pages/Service"
+// import Layout from "./Layout"
+// import Home from "./pages/Home"
+// import About from "./pages/About"
+// import Contact from "./pages/Contact"
+// import Service from "./pages/Service"
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<Layout/>}>
+//         <Route index element={<Home/>}/>
+//         <Route path="about" element={<About/>}/>
+//         <Route path="contact" element={<Contact/>}/>
+//         <Route path="service" element={<Service/>}/>
+
+//       </Route>
+//     </Routes>
+//     </BrowserRouter>
+//     )
+// }
+
+import React from 'react'
+import Counter from './pages/Counter'
+import Effect from './pages/Effect'
+import Reducer from './pages/Reducer'
+import Callback from './pages/Callback'
 
 export default function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
-        <Route path="contact" element={<Contact/>}/>
-        <Route path="service" element={<Service/>}/>
-
-      </Route>
-    </Routes>
-    </BrowserRouter>
-    )
+    <div><Counter/>
+    <Effect/>
+    <Reducer/>
+    <Callback/></div>
+  )
 }
